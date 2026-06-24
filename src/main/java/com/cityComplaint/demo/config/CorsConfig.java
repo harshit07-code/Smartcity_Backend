b066a8925c4af22e30a719f3aedace4f80114a01
harshit07-code <harshit07code@gmail.com>
2026-06-24 18:31:52 +0530

    CORS config updated

--- src/main/java/com/cityComplaint/demo/config/CorsConfig.java
+++ src/main/java/com/cityComplaint/demo/config/CorsConfig.java
@@ -7,6 +7,29 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//@Configuration
+//public class CorsConfig {
+//
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//
+//        return new WebMvcConfigurer() {
+//
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//
+//                registry.addMapping("/**")
+//                        .allowedOrigins("http://localhost:3000")
+//                        .allowedMethods("*");
+//
+//            }
+//        };
+//    }
+//
+//
+//}
+//
+//
 @Configuration
 public class CorsConfig {
 
@@ -19,14 +42,14 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("*");
-
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "https://your-frontend-name.netlify.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
-
-
 }
-
-
